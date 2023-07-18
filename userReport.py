541f0f252bfe04776ab6a12bfaf026556da11a59
Lalit Kumar Jena <lalit@contlo.com>
2023-07-18 19:37:31 +0530

    single user report

--- userReport.py
+++ userReport.py
@@ -30,7 +30,8 @@ class reportTool(BaseTool):
 
     def _execute(self, dim: str, met: str, start: str, end: str):
         os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = "superagi/tools/google-analytics-tool-superagi/ga4api-34c2e.json"
-        pid=int(self.get_tool_config('property_id'))
+        # pid=int(self.get_tool_config('property_id'))
+        pid=376881934
         client = BetaAnalyticsDataClient()
 
         m = self.getMetric(met)
