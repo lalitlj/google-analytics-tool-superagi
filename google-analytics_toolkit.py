3bdb5e41a9d84bcbaafbd920266233eac2e32da2
Lalit Kumar Jena <lalit@contlo.com>
2023-07-18 19:39:25 +0530

    toolkit

--- google-analytics_toolkit.py
+++ google-analytics_toolkit.py
@@ -3,13 +3,14 @@ import os
 from superagi.tools.base_tool import BaseToolkit, BaseTool
 from typing import List
 from userReport import reportTool
+from report import singleUseTool
 
 class AnalyticsToolkit(BaseToolkit, ABC):
     name: str = "Google Analytics Toolkit"
     description: str = "Google Analytics Toolkit returns google analytics reports requested by the user"
 
     def get_tools(self) -> List[BaseTool]:
-        return [reportTool()]
+        return [reportTool(), singleUseTool()]
 
     def get_env_keys(self) -> List[str]:
         return ["GOOGLE_APPLICATION_CREDENTIALS"]
