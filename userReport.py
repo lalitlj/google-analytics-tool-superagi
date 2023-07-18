00bfff153de848db9797b1d2b935aa900c63fcf9
Lalit Kumar Jena <lalit@contlo.com>
2023-07-18 11:44:13 +0530

    Dimensions Metrics trying

--- userReport.py
+++ userReport.py
@@ -13,8 +13,8 @@ from google.analytics.data_v1beta.types import (
 )
 
 class UserReportInput(BaseModel):
-    # met: str = Field(..., description="The metric the user wants to know, for example number of active users")
-    # dim: str = Field(..., description="The context or dimension for which the user wants to know, for example, a city")
+    met: str = Field(..., description="The metric the user wants to know, for example number of active users")
+    dim: str = Field(..., description="The context or dimension for which the user wants to know, for example, a city")
     start: str = Field(..., description="The starting date of the query, in YYYY-MM-DD format")
     end: str = Field(..., description=f"The last date of the query, in YYYY-MM-DD format, if today, return today's date")
 
@@ -27,27 +27,27 @@ class reportTool(BaseTool):
     args_schema: Type[BaseModel] = UserReportInput
     description: str = "Return a google analytics report for the information the user requires"
 
-    def _execute(self, start: str, end: str):
+    def _execute(self, dim: str, met: str, start: str, end: str):
         os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = "superagi/tools/google-analytics-tool-superagi/ga4api-34c2e.json"
         # pid=self.get_tool_config('property_id'
         pid=376881934
         client = BetaAnalyticsDataClient()
 
-        # m = getMetric(met)
-        # d = getDim(dim)
-        # mi=[]
-        # for x in m:
-        #     mi.append(Metric(name=x))
-        # if len(m)==0:
-        #     return "No metric found"
-        # di = []
-        # for x in d:
-        #     di.append(Dimension(name=x))
-        #     return "No dimension found"
+        m = getMetric(met)
+        d = getDim(dim)
+        mi=[]
+        for x in m:
+            mi.append(Metric(name=x))
+        if len(m)==0:
+            return "No metric found"
+        di = []
+        for x in d:
+            di.append(Dimension(name=x))
+            return "No dimension found"
         request = RunReportRequest(
             property=f"properties/{pid}",
-            dimensions=[Dimension(name="city")],
-            metrics=[Metric(name="activeUsers")],
+            dimensions=di,
+            metrics=mi,
             date_ranges=[DateRange(start_date=start, end_date=end)],
         )
         response = client.run_report(request)
